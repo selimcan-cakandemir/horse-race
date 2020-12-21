d338a3442a165e2a486a8223524d900012498d60
Selimcan <sccdemir@gmail.com>
2020-12-21 11:19:24 +0300

    first commit

---
+++ WFA_HorseRace/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿
+namespace WFA_HorseRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbAt1 = new System.Windows.Forms.PictureBox();
+            this.pbAt2 = new System.Windows.Forms.PictureBox();
+            this.pbAt3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSpiker = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblVictory = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAt1
+            // 
+            this.pbAt1.BackColor = System.Drawing.Color.Transparent;
+            this.pbAt1.Image = ((System.Drawing.Image)(resources.GetObject("pbAt1.Image")));
+            this.pbAt1.Location = new System.Drawing.Point(22, 83);
+            this.pbAt1.Name = "pbAt1";
+            this.pbAt1.Size = new System.Drawing.Size(83, 61);
+            this.pbAt1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbAt1.TabIndex = 0;
+            this.pbAt1.TabStop = false;
+            // 
+            // pbAt2
+            // 
+            this.pbAt2.BackColor = System.Drawing.Color.Transparent;
+            this.pbAt2.Image = ((System.Drawing.Image)(resources.GetObject("pbAt2.Image")));
+            this.pbAt2.Location = new System.Drawing.Point(22, 186);
+            this.pbAt2.Name = "pbAt2";
+            this.pbAt2.Size = new System.Drawing.Size(83, 61);
+            this.pbAt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbAt2.TabIndex = 0;
+            this.pbAt2.TabStop = false;
+            // 
+            // pbAt3
+            // 
+            this.pbAt3.BackColor = System.Drawing.Color.Transparent;
+            this.pbAt3.Image = ((System.Drawing.Image)(resources.GetObject("pbAt3.Image")));
+            this.pbAt3.Location = new System.Drawing.Point(22, 290);
+            this.pbAt3.Name = "pbAt3";
+            this.pbAt3.Size = new System.Drawing.Size(83, 61);
+            this.pbAt3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbAt3.TabIndex = 0;
+            this.pbAt3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(-2, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1106, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(1, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1103, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.BackColor = System.Drawing.Color.DarkRed;
+            this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinish.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblFinish.Location = new System.Drawing.Point(1039, 59);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(65, 336);
+            this.lblFinish.TabIndex = 2;
+            this.lblFinish.Text = "FINISH";
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Enabled = false;
+            this.btnBaslat.Location = new System.Drawing.Point(12, 538);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(160, 33);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSpiker
+            // 
+            this.lblSpiker.Location = new System.Drawing.Point(212, 538);
+            this.lblSpiker.Name = "lblSpiker";
+            this.lblSpiker.Size = new System.Drawing.Size(296, 88);
+            this.lblSpiker.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.nud3);
+            this.groupBox1.Controls.Add(this.nud2);
+            this.groupBox1.Controls.Add(this.nud1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(540, 538);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 93);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Yatır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Yatır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Yatır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nud3
+            // 
+            this.nud3.Location = new System.Drawing.Point(149, 42);
+            this.nud3.Name = "nud3";
+            this.nud3.Size = new System.Drawing.Size(39, 20);
+            this.nud3.TabIndex = 5;
+            // 
+            // nud2
+            // 
+            this.nud2.Location = new System.Drawing.Point(79, 42);
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(39, 20);
+            this.nud2.TabIndex = 5;
+            // 
+            // nud1
+            // 
+            this.nud1.Location = new System.Drawing.Point(9, 42);
+            this.nud1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(39, 20);
+            this.nud1.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(146, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 23);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "1.80";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(146, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "3. At";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(76, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 23);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "1.50";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(6, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 23);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "1.30";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(76, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "2. At";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "1. At";
+            // 
+            // lblVictory
+            // 
+            this.lblVictory.Location = new System.Drawing.Point(940, 555);
+            this.lblVictory.Name = "lblVictory";
+            this.lblVictory.Size = new System.Drawing.Size(138, 45);
+            this.lblVictory.TabIndex = 6;
+            this.lblVictory.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(216, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 56);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1102, 643);
+            this.Controls.Add(this.lblVictory);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblSpiker);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbAt3);
+            this.Controls.Add(this.pbAt2);
+            this.Controls.Add(this.pbAt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAt1;
+        private System.Windows.Forms.PictureBox pbAt2;
+        private System.Windows.Forms.PictureBox pbAt3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSpiker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown nud3;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblVictory;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WFA_HorseRace/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_HorseRace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        decimal bet1;
+        decimal bet2;
+        decimal bet3;
+
+        decimal pot;
+
+        decimal multi1 = 1.30m;
+        decimal multi2 = 1.50m;
+        decimal multi3 = 1.80m;
+
+        
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //Movement section
+            pbAt1.Left += rnd.Next(1, 30);
+            pbAt2.Left += rnd.Next(1, 30);
+            pbAt3.Left += rnd.Next(1, 30);
+            //this is for manipulating the location. keep in mind .Right is a read-only value, we have to use "-". For now we'll send the random number
+
+            //Victory message
+            if (pbAt1.Right >= lblFinish.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("1 wins");
+                pot = bet1 * multi1;
+                lblVictory.Text = pot.ToString() + " kazanıldı!";
+            }
+            else if (pbAt2.Right >= lblFinish.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("2 wins");
+                pot = bet2 * multi2;
+                lblVictory.Text = pot.ToString() + " kazanıldı!";
+            }
+            else if (pbAt3.Right >= lblFinish.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("3 wins");
+                pot = bet3 * multi3;
+                lblVictory.Text = pot.ToString() + " kazanıldı!";
+            }
+            
+            //Announcer section
+
+            //1 ahead
+            else if (pbAt1.Right > pbAt2.Right)
+            {
+                if (pbAt1.Right > pbAt3.Right)
+                {
+                    lblSpiker.Text = "1 önde!";
+                }
+            }
+
+            //2 ahead
+            else if (pbAt2.Right > pbAt1.Right)
+            {
+                if (pbAt2.Right > pbAt3.Right)
+                {
+                    lblSpiker.Text = "2 önde!";
+                }
+            }
+
+            //3 ahead
+            else if (pbAt3.Right > pbAt1.Right)
+            {
+                if (pbAt3.Right > pbAt2.Right)
+                {
+                    lblSpiker.Text = "3 önde!";
+                }
+            }
+
+        }
+
+         //Beting part
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bet1 = nud1.Value;
+            listBox1.Items.Add(bet1.ToString() + " puan 1'e yatırıldı!");
+            btnBaslat.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bet2 = nud2.Value;
+            listBox1.Items.Add(bet2.ToString() + " puan 2'e yatırıldı!");
+            btnBaslat.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bet3 = nud3.Value;
+            listBox1.Items.Add(bet3.ToString() + " puan 3'e yatırıldı!");
+            btnBaslat.Enabled = true;
+        }
+    }
+}
